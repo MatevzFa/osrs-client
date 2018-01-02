7fb3b62676a3f6a0ede8735b2046653faed7571c
Matevž Fabjančič <mf6422@student.uni-lj.si>
2018-01-02 23:19:17 +0100

    install.ps1 cleanup

--- install.ps1
+++ install.ps1
@@ -1,3 +1,8 @@
+<# 
+    Builds and installs Old School RuneScape client to a Windows machine.
+    The client is installed to %HOME%\.osrs-client\ folder by default.
+#>
+
 
 $INSTALL_DIR = $HOME + "\.osrs-maven\"
 
@@ -8,14 +13,18 @@ if (!(Test-Path $INSTALL_DIR)) {
     New-Item -ItemType Directory -Path $INSTALL_DIR
 }
 
+$jarName = Get-ChildItem -Path .\target\osrsclient-maven-*.jar -Name
+$iconName = 'osrs.ico'
+
 # Copy the client to the location
-Copy-Item -Path ".\target\osrsclient-maven-1.0-SNAPSHOT.jar" -Destination $INSTALL_DIR
-Copy-Item -Path ".\src\main\resources\icon_128.ico" -Destination $INSTALL_DIR
+Copy-Item -Path ".\target\$jarName" -Destination $INSTALL_DIR
+Copy-Item -Path ".\src\main\resources\$iconname" -Destination $INSTALL_DIR
 
 # Create an entry in Start Menu
 $objShell = New-Object -ComObject ("WScript.Shell")
-$objShortCut = $objShell.CreateShortcut($env:USERPROFILE + '\AppData\Roaming\Microsoft\Windows\Start Menu\Programs' + '\RuneScape' + '\Old School RuneScape.lnk')
-$jarPath = $INSTALL_DIR + 'osrsclient-maven-1.0-SNAPSHOT.jar'
-$objShortCut.TargetPath = $jarPath
-$objShortCut.IconLocation = $INSTALL_DIR + 'icon_128.ico'
+$objShortCut = $objShell.CreateShortcut($env:USERPROFILE + '\AppData\Roaming\Microsoft\Windows\Start Menu\Programs' + '\Old School RuneScape.lnk')
+
+$objShortCut.TargetPath = $INSTALL_DIR + $jarName
+$objShortCut.IconLocation = $INSTALL_DIR + $iconName
+
 $objShortCut.Save()
