98662d41ff47dca101aa567612b6a8b51c0781e1
Matevž Fabjančič <mf6422@student.uni-lj.si>
2018-02-02 08:37:32 +0100

    Revised window minimum size

--- src/main/java/com/matevzfa/osrsclient/rsclient/coregui/RSClient.java
+++ src/main/java/com/matevzfa/osrsclient/rsclient/coregui/RSClient.java
@@ -7,8 +7,6 @@ import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.applet.Applet;
 import java.awt.*;
-import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.util.stream.Collectors;
@@ -32,42 +30,37 @@ public class RSClient {
 
         JFrame mainwnd = new JFrame("Old School RuneScape");
 
-        mainwnd.setIconImages(
-                Stream.of("icon_16.png", "icon_32.png", "icon_64.png", "icon_128.png")
-                        .map(RSClient::imageFromFile)
-                        .collect(Collectors.toList()));
+        mainwnd.setIconImages(Stream.of("icon_16.png", "icon_32.png", "icon_64.png", "icon_128.png")
+                                    .map(RSClient::imageFromFile)
+                                    .collect(Collectors.toList()));
 
         mainwnd.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-//        mainwnd.getContentPane().setMinimumSize(new Dimension(GAME_WIDTH, GAME_HEIGHT));
-        mainwnd.setMinimumSize(new Dimension(GAME_WIDTH, GAME_HEIGHT));
+        mainwnd.getContentPane().setMinimumSize(new Dimension(GAME_WIDTH, GAME_HEIGHT));
 
         JPanel mainpanel = new JPanel(new GridLayout());
         mainpanel.setBackground(Color.black);
         mainpanel.setMinimumSize(new Dimension(GAME_WIDTH, GAME_HEIGHT));
 
-        mainwnd.getContentPane().add(mainpanel);
-
-        mainpanel.setVisible(true);
-
+        mainwnd.add(mainpanel);
+        mainwnd.getContentPane().setMinimumSize(new Dimension(GAME_WIDTH, GAME_HEIGHT));
         mainwnd.pack();
+
+        Insets insets = mainwnd.getInsets();
+        mainwnd.setMinimumSize(new Dimension(GAME_WIDTH + insets.left + insets.right,
+                                             GAME_HEIGHT + insets.top + insets.bottom));
 
         mainwnd.setVisible(true);
 
         try {
             final Loader loader = new Loader();
-            // mainpanel.add(loader.getApplet());
             Applet applet = loader.getApplet();
-            // so it doesn't hang on Ubuntu (tested on 17.10)
+            // sleep for 0.5s so it doesn't hang on Ubuntu (tested on 17.10)
             Thread.sleep(500);
             mainpanel.add(applet);
             loader.getApplet().resize(GAME_WIDTH, GAME_HEIGHT);
         } catch (IllegalArgumentException e) {
             e.printStackTrace();
         }
-
-        int gameX = mainwnd.getComponent(0).getX();
-        int gameY = mainwnd.getComponent(0).getY();
-        mainwnd.setMinimumSize(new Dimension(GAME_WIDTH + gameX, GAME_HEIGHT + gameY));
     }
 
     private static BufferedImage imageFromFile(String imgName) {
